09909d9393ded6688d0e67be5a4f9785b2ca6c4e
unknown <Sviatoslav_Chetyrkin@EPUALVIW0429.kyiv.epam.com>
2013-06-25 13:31:06 +0300

    deleted unnecesary files

--- target/classes/sql_scripts/20130528_initialInfo.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO medis.user (login, password, role) VALUES ('admin', '111', 'ROLE_ADMIN');
-INSERT INTO medis.user (login, password, role) VALUES ('user', '111', 'ROLE_USER');
-
-INSERT INTO medis.patient (name, surname, fathersName, sex) VALUES ('vasa', 'vasa', 'vasa', '1');
-INSERT INTO medis.patient (name, surname, fathersName, sex) VALUES ('ira', 'ira', 'ira', '0');
-
-
