ba03bfa013a7fcecc1ffd66988b15b6ccfde1494
unknown <Sviatoslav_Chetyrkin@EPUALVIW0429.kyiv.epam.com>
2013-07-01 17:57:19 +0300

    Doctor entity added.

---
+++ src/main/java/com/chetyrkin/medis/domain/Doctor.java
@@ -0,0 +1,165 @@
+package com.chetyrkin.medis.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="doctor")
+public class Doctor {
+
+	@Id
+    @GeneratedValue
+    @Column(name="id")
+    private Long id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="surname")
+	private String surname;
+	
+	@Column(name="fathersName")
+	private String fathersName;
+	
+	@Column(name="profession")
+	private String profession;
+	
+	@Column(name="assignment")
+	private String assignment;
+	
+	@Column(name="department")
+	private String department;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getFathersName() {
+		return fathersName;
+	}
+
+	public void setFathersName(String fathersName) {
+		this.fathersName = fathersName;
+	}
+
+	public String getProfession() {
+		return profession;
+	}
+
+	public void setProfession(String profession) {
+		this.profession = profession;
+	}
+
+	public String getAssignment() {
+		return assignment;
+	}
+
+	public void setAssignment(String assignment) {
+		this.assignment = assignment;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((assignment == null) ? 0 : assignment.hashCode());
+		result = prime * result
+				+ ((department == null) ? 0 : department.hashCode());
+		result = prime * result
+				+ ((fathersName == null) ? 0 : fathersName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((profession == null) ? 0 : profession.hashCode());
+		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Doctor other = (Doctor) obj;
+		if (assignment == null) {
+			if (other.assignment != null)
+				return false;
+		} else if (!assignment.equals(other.assignment))
+			return false;
+		if (department == null) {
+			if (other.department != null)
+				return false;
+		} else if (!department.equals(other.department))
+			return false;
+		if (fathersName == null) {
+			if (other.fathersName != null)
+				return false;
+		} else if (!fathersName.equals(other.fathersName))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (profession == null) {
+			if (other.profession != null)
+				return false;
+		} else if (!profession.equals(other.profession))
+			return false;
+		if (surname == null) {
+			if (other.surname != null)
+				return false;
+		} else if (!surname.equals(other.surname))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Doctor [id=" + id + ", name=" + name + ", surname=" + surname
+				+ ", fathersName=" + fathersName + ", profession=" + profession
+				+ ", assignment=" + assignment + ", department=" + department
+				+ "]";
+	}
+	
+}
