5d2c02cdf9b182daea8dbd44df40fe6888dced51
unknown <Sviatoslav_Chetyrkin@EPUALVIW0429.kyiv.epam.com>
2013-07-03 15:32:18 +0300

    Fixed DoctorDAO bug. Added event entity.

--- src/main/java/com/chetyrkin/medis/dao/impl/DoctorDAOImpl.java
+++ src/main/java/com/chetyrkin/medis/dao/impl/DoctorDAOImpl.java
@@ -1,8 +1,11 @@
 package com.chetyrkin.medis.dao.impl;
 
+import org.springframework.stereotype.Repository;
+
 import com.chetyrkin.medis.dao.DoctorDAO;
 import com.chetyrkin.medis.domain.Doctor;
 
+@Repository("doctorDAO")
 public class DoctorDAOImpl extends GenericDAOImpl<Doctor, Long> implements DoctorDAO {
 
 	protected DoctorDAOImpl() {
