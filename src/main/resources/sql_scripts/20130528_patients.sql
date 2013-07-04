6c13c48b69380fb60fa2e5034f30a7e556e00206
unknown <Sviatoslav_Chetyrkin@EPUALVIW0429.kyiv.epam.com>
2013-07-04 17:20:17 +0300

    Charset bug fix.

--- src/main/resources/sql_scripts/20130528_patients.sql
+++ src/main/resources/sql_scripts/20130528_patients.sql
@@ -1,15 +1,15 @@
 CREATE TABLE medis.patient (
     id BIGINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
-    name VARCHAR(20) NOT NULL,
-    surname VARCHAR(20) NOT NULL,
-    fathersName VARCHAR(20) NULL,
+    name VARCHAR(20) CHARACTER SET utf8 NOT NULL,
+    surname VARCHAR(20) CHARACTER SET utf8 NOT NULL,
+    fathersName VARCHAR(20) CHARACTER SET utf8 NULL,
     bloodType enum('Ap', 'An', 'Op', 'On', 'Bp', 'Bn', 'ABp', 'ABn') NULL,
     sex BOOL NOT NULL,
     birthDate DATE NULL,
     telephone BIGINT NULL,
-    workAdress VARCHAR(50) NULL,
-    liveAdress VARCHAR(50) NULL,
-    specialAccounting VARCHAR(80) NULL,
+    workAdress VARCHAR(50) CHARACTER SET utf8 NULL,
+    liveAdress VARCHAR(50) CHARACTER SET utf8 NULL,
+    specialAccounting VARCHAR(80) CHARACTER SET utf8 NULL,
     hiv BOOL NULL,
     PRIMARY KEY (id)
 )  ENGINE=INNODB;
--- src/main/resources/sql_scripts/20130703_event.sql
+++ src/main/resources/sql_scripts/20130703_event.sql
@@ -2,12 +2,12 @@ CREATE TABLE medis.event (
 	id BIGINT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
 	patient_id BIGINT UNSIGNED NOT NULL,
 	date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	clinic_name VARCHAR(20) NOT NULL,
+	clinic_name VARCHAR(20) CHARACTER SET utf8 NOT NULL,
 	hours_after_accident INT NULL,
 	hospitalized_in_planned_order BOOL NOT NULL,
-	hospitalization_diagnosis VARCHAR(50) NULL,
-	facility_diagnosis VARCHAR(50) NULL,
-	clinical_diagnosis VARCHAR(50) NULL,
+	hospitalization_diagnosis VARCHAR(50) CHARACTER SET utf8 NULL,
+	facility_diagnosis VARCHAR(50) CHARACTER SET utf8 NULL,
+	clinical_diagnosis VARCHAR(50) CHARACTER SET utf8 NULL,
 	FOREIGN KEY (patient_id) REFERENCES patient(id),
 	PRIMARY KEY (id)
 )ENGINE=INNODB CHARACTER SET utf8 COLLATE utf8_general_ci;
