4cf9cde740c9037cd9672361ed9fd433aae5d844
DancerInTheDark <palach.t@gmail.com>
2013-08-23 23:30:28 +0300

    Changed the view of date on the medical card's page.

--- src/main/webapp/js/medicalCard.js
+++ src/main/webapp/js/medicalCard.js
@@ -13,10 +13,11 @@ function addNewMedicalCard(e) {
 function fillMedicalCardTable(medicalCards) {
 	$("#medical_cards_table tbody").empty();
 	$.each(medicalCards, function() {
+		var date = new Date(this.date);
 		var varId = this.id;
 		var medicalCard_id = $("<td id='medicalCard_id'></td>").text(this.id);
 		var medicalCard_fullname= $("<td></td>").text(this.patient.name + " " + this.patient.surname + " " + this.patient.fathersName);
-		var medicalCard_date = $("<td></td>").text(this.date);
+		var medicalCard_date = $("<td></td>").text($.datepicker.formatDate('dd - mm - yy', date));
 		var medicalCard_downloadButton = $("<td><button class='btn btn-info' type='button'>Завантажити карточку</button></td>"); 
 		medicalCard_downloadButton.click(function(){
 				document.location = "http://" + document.location.host + "/medis/medicalCard/" + varId + "/getFile.pdf";
